089df2249ef3bbe66a2f0e982673a9c0a72e7b4d
Hugh <hmsti1@student.monash.edu>
2018-05-11 23:14:07 +1000

    Testing fixes
    Code is now in a working state ready to be further tested with new plot types

--- matlab processing files/main.m
+++ matlab processing files/main.m
@@ -6,17 +6,35 @@ file_path = '..\output .dat files\diverging\Re1.dat';
 [variables, data] = extract_data(file_path);
 
 %% Slice the data
-
+slice_vals = 0:1:10;
+n_slices = length(slice_vals);
+slice_col = 1; %slice by x values
+sliced_data = slice_data(data, slice_col, slice_vals);
 
 
 %% DO something with the data
+
+upper_boundary = zeros(n_slices,1);
+lower_boundary = upper_boundary;
+
+for i = 1:n_slices
+	this_slice = sliced_data(i,:);
+	
+	
+	upper_boundary(i) = max(this_slice{2});
+	lower_boundary(i) = min(this_slice{2});
+end
+
+
 figure
 hold on
 set(gca,'XAxisLocation','origin')
-daspect([1 0.5 1])
-plot(values,upper_boundary,'k')
-plot(values,lower_boundary,'k')
+%daspect([1 0.5 1])
+plot(slice_vals,upper_boundary,'k')
+plot(slice_vals,lower_boundary,'k')
 
+
+%{
 %Loop through each slice
 for j = 1:n_slices
 	this_slice = vertical_slices{j};
@@ -33,5 +51,5 @@ for j = 1:n_slices
 	hold on
 end
 
-
+%}
 %%%%%%%% Should try streamslice function on full data set
--- matlab processing files/slice_data.m
+++ matlab processing files/slice_data.m
@@ -8,9 +8,8 @@ function [ output ] = slice_data(data, slice_col, slice_vals)
 %
 %
 % input/output format: cell array with format:
-%{[x_col], [y_col], [u_col], [v_col], [p_col]} % at slice value 1
-%{[x_col], [y_col], [u_col], [v_col], [p_col]} % at slice value 2
-
+%{[x_col], [y_col], [u_col], [v_col], [p_col]} % at slice_val 1
+%{[x_col], [y_col], [u_col], [v_col], [p_col]} % at slice_val 2
 
 
 n_vars = size(data);
@@ -20,64 +19,21 @@ n_slices = length(slice_vals);
 sliced_cell = cell(n_slices,n_vars);
 
 
-
-%% Find the rows
-% find indices of the rows which have a member of slice_vals in their slice_col
-
 for i = 1:n_slices
 	slice_val = slice_vals(i);
 	
-	 %Finding the values in column slice_col
+	% find indices of the rows which have a member of slice_vals in their slice_col
 	slice = data{slice_col}(:)== slice_val;
-	
 	%creates a logical column vector with 1 at the indices of values that
 	%satisfy the condition
 	
+	
+	%loop through and add column for each variable 
 	for j = 1:n_vars
 		sliced_cell{i,j} = data{j}(slice);
 	end
-
 end
 
-
-
-
-%% Extract the rows at indices found above
-
-
-
-
-
-%%
-%{
-upper_boundary = zeros(n_slices,1);
-lower_boundary = upper_boundary;
-vertical_slices = cell(n_slices,1);
-
-% Separate and store vertical slices nicely
-
-for i = 1:n_slices
-	value = values(i);
-	x_logical = X == value;
-	%Extract boundary points
-	upper_boundary(i) = max(Y(x_logical));
-	lower_boundary(i) = min(Y(x_logical));
-	
-	
-	%store each vertical slice in a cell array
-	vertical_slice = zeros(1,n_vars);
-	
-	vertical_slice(1:length(X(x_logical)),1) = X(x_logical);
-	vertical_slice(1:length(Y(x_logical)),2) = Y(x_logical);
-	vertical_slice(1:length(U(x_logical)),3) = U(x_logical);
-	vertical_slice(1:length(V(x_logical)),4) = V(x_logical);
-	vertical_slice(1:length(P(x_logical)),5) = P(x_logical);
-	
-	vertical_slices{i} = vertical_slice;
-	
-	
-end
-%}
 
 output = sliced_cell;
 end
