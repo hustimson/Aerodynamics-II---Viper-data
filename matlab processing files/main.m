610adc1645003e0f1251ce39be3d8010cdc4a2b3
Hugh <hmsti1@student.monash.edu>
2018-05-12 02:39:38 +1000

    Slice now has non exact option

--- matlab processing files/main.m
+++ matlab processing files/main.m
@@ -3,6 +3,11 @@ close all; clc
 
 
 %% Import data
+
+
+%allow user to choose a .dat file to analyse, or for ease of debug use a
+%pre-entered file
+
 choose = 0;
 if choose
 	[file_name, file_path] = uigetfile('..\output .dat files\.dat');
@@ -19,6 +24,7 @@ end
 
 
 %% Slice the data
+%Because I don't wanna be working with 13000 row vectors
 slice_vals = 0:1:10;
 n_slices = length(slice_vals);
 slice_col = 1; %slice by x values
@@ -27,11 +33,11 @@ sliced_data = slice_data(data, slice_col, slice_vals);
 
 %% Flow-wise quantity extraction
 
-upper_boundary = zeros(n_slices,1);
-lower_boundary = upper_boundary;
+upper_wall = zeros(n_slices,1);
+lower_wall = upper_wall;
 
-upper_BL = upper_boundary;
-lower_BL = upper_boundary;
+upper_BL = upper_wall;
+lower_BL = upper_wall;
 
 
 for i = 1:n_slices
@@ -43,8 +49,8 @@ for i = 1:n_slices
 	v = cell2mat(this_slice(4));
 	p = cell2mat(this_slice(5));
 	
-	upper_boundary(i) = max(y);
-	lower_boundary(i) = min(y);
+	upper_wall(i) = max(y);
+	lower_wall(i) = min(y);
 	
 	%find greatest u velocity less than 0.2 away from duct centre
 	centre_velocity = max(u(abs(y)<0.05)); 
@@ -68,10 +74,14 @@ end
 figure
 hold on
 set(gca,'XAxisLocation','origin')
+set(gca,'Visible','off')
 title(file_string, 'Interpreter', 'none')
 %daspect([1 0.5 1])
-plot(slice_vals,upper_boundary,'k')
-plot(slice_vals,lower_boundary,'k')
+plot(slice_vals,upper_wall,'k')
+plot(slice_vals,lower_wall,'k')
+[y_corner, x_corner] = max(upper_wall); 
+fill([slice_vals, abs(11-x_corner)],[lower_wall; -y_corner],[0.5 0.5 0.5]);
+fill([slice_vals, abs(11-x_corner)],[upper_wall;  y_corner],[0.5 0.5 0.5]);
 
 plot(slice_vals, upper_BL,'m')
 plot(slice_vals, lower_BL,'m')
@@ -79,8 +89,9 @@ plot(slice_vals, lower_BL,'m')
 
 %% Span-wise analysis 
 %Loop through each slice
-for j = 1:n_slices
+for j = 1:n_slices-1
 	this_slice = sliced_data(j,:);
+	
 	
 	x = cell2mat(this_slice(1));
 	y = cell2mat(this_slice(2));
@@ -90,9 +101,9 @@ for j = 1:n_slices
 	
 	
 	%Plotting velocity profile
-% 	quiver(x,y,u/4,zeros(size(u)),'b','MaxHeadSize',0.1,'AutoScale', 'off')
-% 	plot(x+u/4,y,'r')
-% 	plot(x,y ,'b')
+	quiver(x,y,u/4,zeros(size(u)),'b','MaxHeadSize',0.1,'AutoScale', 'off')
+ 	plot(x+u/4,y,'r')
+	plot(x,y ,'b')
 end
 
 %}
--- matlab processing files/slice_data.m
+++ matlab processing files/slice_data.m
@@ -3,7 +3,14 @@ function [ output ] = slice_data(data, slice_col, slice_vals)
 % only rows with values: slice_vals in column: slice_col
 %
 % because we don't want every point as it would clutter the analysis
+%
+% if slice_vals is a vector, find exact values
+% otherwise slice_vals should be a cell where the first element is the
+% range of indices the slice desired, and the second element is the number of
+% points desired in the range
 
+
+%
 % example: all row where the x value is one of the elements in [0:1:10]
 %
 %
@@ -14,27 +21,55 @@ function [ output ] = slice_data(data, slice_col, slice_vals)
 
 n_vars = size(data);
 n_vars = n_vars(2);
-n_slices = length(slice_vals);
-
-sliced_cell = cell(n_slices,n_vars);
 
 
 
-for i = 1:n_slices
-	slice_val = slice_vals(i);
+% case where exact values are desired (integers basically)
+if  isa(slice_vals, 'double')
 	
-	% find indices of the rows which have a member of slice_vals in their slice_col
-	slice = data{slice_col}(:) == slice_val;
-	%creates a logical column vector with 1 at the indices of values that
-	%satisfy the condition
+	n_slices = length(slice_vals);
+	sliced_cell = cell(n_slices,n_vars);
 	
+	for i = 1:n_slices
+		slice_val = slice_vals(i);
+		
+		% find indices of the rows which have a member of slice_vals in their slice_col
+		slice_logical = data{slice_col}(:) == slice_val;
+		%creates a logical column vector with 1 at the indices of values that
+		%satisfy the condition
+		
+		
+		%loop through and add column for each variable
+		for j = 1:n_vars
+			sliced_cell{i,j} = data{j}(slice_logical);
+		end
+	end
 	
-	%loop through and add column for each variable 
-	for j = 1:n_vars
-		sliced_cell{i,j} = data{j}(slice);
+%case where we are just picking values with a linear spacing 	
+else
+	if isa(slice_vals, 'cell')
+		
+	n_slices = slice_vals{2};
+	sliced_cell = cell(n_slices,n_vars);
+		
+	bottom = slice_vals{1}(1);
+	top = slice_vals{1}(2);
+	range = top-bottom;
+	
+	indices = round(linspace(bottom,top,n_slices));
+	slice_logical = zeros(range,1);
+	slice_logical(indices) = 1;
+	
+	for i = 1:n_slices
+		for j = 1:n_vars
+			temp_range = data{j}(bottom:top);
+			
+			sliced_cell{i,j} = temp_range(logical(slice_logical));
+		end
+	end
+	
 	end
 end
-
-
+	
 output = sliced_cell;
 end
