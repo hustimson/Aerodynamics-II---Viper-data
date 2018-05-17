1dfcbe48ccec4905eb80f23b70020542ddd80db0
Hugh <hmsti1@student.monash.edu>
2018-05-17 13:51:07 +1000

    Hopefully last commit

--- matlab processing files/BL_vs_reynolds_plots.m
+++ matlab processing files/BL_vs_reynolds_plots.m
@@ -1,9 +1,6 @@
 % Plotting BL thickness vs x for various reynolds numbers
 
-%%
-
-
-section = 'Diverg';
+section = 'Straight';
 
 switch section
 	case 'Converg'
@@ -20,12 +17,10 @@ switch section
 		plot(high_res_slice_vals,re1000, 'DisplayName','Re = 10^3')
 		plot(high_res_slice_vals,re10000,'DisplayName','Re = 10^4')
 		
-		legend('show','location','southeast')
-
-%%
-% 
-	case 'Diverg'
+		legend('show','location','northeast')
+		title('Boundary Layer profiles, Converging duct');
 		
+	case 'Diverg'
 		re1     = importdata([section,'N8Re_10E0 BL thickness.txt']);
 		re10    = importdata([section,'N8Re_10E1 BL thickness.txt']);
 		re100   = importdata([section,'N8Re_10E2 BL thickness.txt']);
@@ -40,6 +35,28 @@ switch section
 		plot(high_res_slice_vals,re1000, 'DisplayName','Re = 10^3')
 
 		legend('show','location','southeast')
+		title('Boundary Layer profiles, Diverging duct');
+		
+	case 'Straight'	
+		re1     = importdata([section,'N8Re_10E0 BL thickness.txt']);
+		re10    = importdata([section,'N8Re_10E1 BL thickness.txt']);
+		re100   = importdata([section,'N8Re_10E2 BL thickness.txt']);
+		re500   = importdata([section,'N8Re_5x10E2 BL thickness.txt']);
+		re1000  = importdata([section,'N8Re_10E3 BL thickness.txt']);
+		
+		hold on
+		plot(high_res_slice_vals,re1,    'DisplayName', 'Re = 10^0')
+		plot(high_res_slice_vals,re10,   'DisplayName', 'Re = 10^1')
+		plot(high_res_slice_vals,re100,  'DisplayName', 'Re = 10^2')
+		plot(high_res_slice_vals,re500,  'DisplayName', 'Re = 500')
+		plot(high_res_slice_vals,re1000, 'DisplayName', 'Re = 10^3')
+
+		legend('show','location','southeast')		
+		title('Boundary Layer profiles, Straight duct');
 end
 
+xlabel('x location')
+ylabel('Boundary layer thickness (\delta)')
+
+saveas(gcf,['..\MATLAB plots\',section,'BL profile vs Re.png']);
 
--- matlab processing files/main.m
+++ matlab processing files/main.m
@@ -13,11 +13,18 @@ close all; clc
 %allow user to choose a .dat file to analyse, or for ease of debug, use a
 %pre-entered file
 
-choose = 1;
 if choose
 	[file_name, file_path] = uigetfile('..\output .dat files\.dat');
 	file_path = [file_path, file_name];
-	[variables, data] = extract_data(file_path);
+	
+	try
+		[variables, data] = extract_data(file_path);
+	catch
+		fprintf('Failed to open file\n')
+		return
+	end
+		
+	
 	file_string = file_name(1:end-4);
 
 else
@@ -35,20 +42,20 @@ n_slices = length(slice_vals);
 slice_col = 1; %slice by x values
 sliced_data = slice_data(data, slice_col, slice_vals);
 
-n_high_res = 80;
+n_high_res = 30;
 [high_res_x_slice, high_res_slice_vals] = slice_data(data, 1, {[0 10], n_high_res});
 
 %% Flow-wise quantity extraction
 %%%%%%%%%%%%%%%% high res section
 
-figure
+visualiser_fig = figure;
 hold on
 set(gca,'XAxisLocation','origin')
 set(gca,'Visible','off')
 
 
 lower_BL = zeros(n_high_res,1);
-lower_BL = lower_BL;
+upper_BL = lower_BL;
 
 upper_wall = zeros(n_high_res,1);
 lower_wall = upper_wall;
@@ -78,15 +85,15 @@ plot(high_res_slice_vals, lower_wall, 'k')
 %fill in wall sections
 [y_corner, y_corner_index] = max(upper_wall);
 if y_corner == 0.5
-	x_corner = 0;
-else
 	x_corner = 10;
+else
+	x_corner = 0;
 end
-fill([high_res_slice_vals, abs(10-x_corner)],[lower_wall; -y_corner],[0.5 0.5 0.5]);
-fill([high_res_slice_vals, abs(10-x_corner)],[upper_wall;  y_corner],[0.5 0.5 0.5]);
+fill( [0,high_res_slice_vals, 10], [-y_corner-0.2; lower_wall; -y_corner-0.2],[0.5 0.5 0.5]);
+fill( [0,high_res_slice_vals, 10], [ y_corner+0.2; upper_wall;  y_corner+0.2],[0.5 0.5 0.5]);
 
 % Print name of file being analysed on the plot
-text(4,y_corner-0.1,file_string, 'Interpreter', 'none')
+text(3,y_corner + 0.1,file_string, 'Interpreter', 'none')
 
 %plot the boundary layer profiles
 smoothing_edge = round(n_high_res*0.1);
@@ -94,7 +101,7 @@ plot(high_res_slice_vals, lower_BL,'m')
 plot(high_res_slice_vals, upper_BL,'m')
 
 %write bl thickness to file
-BL_thickness = upper_wall - upper_BL';
+BL_thickness = upper_wall - upper_BL;
 file_ID = fopen([file_string, ' BL thickness.txt'],'w');
 fprintf(file_ID,'%f\r\n', BL_thickness);
 fclose(file_ID);
@@ -118,14 +125,19 @@ for j = 1:n_slices-1
 
 end
 
+
+
 pressure_fig = figure;
 
 plot(high_res_slice_vals, streamwise_pressure);
-title('streamwise pressure')
+title(sprintf('Streamwise Pressure, %s',file_string),'interpreter','none')
 xlabel('x')
-ylabel('y')
+ylabel('pressure')
+set(gca,'XAxisLocation','origin')
 
 
+saveas(visualiser_fig, ['..\MATLAB plots\',file_string,' flow visualised.png']);
+saveas(pressure_fig,   ['..\MATLAB plots\',file_string,' streamwise pressure.png']);
 
 
 fclose all;
